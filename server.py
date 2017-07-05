30f4246ca1ae59f6324cc1e32af4ce218d6dc10c
Navjot Singh <weavebytes@gmail.com>
2017-07-05 19:29:43 +0530

    added toek required decorator

--- server.py
+++ server.py
@@ -7,11 +7,31 @@ app = Flask(__name__)
 
 app.config['secretkey'] = 'some-strong+secret#key'
 
+def token_required(f):
+    @wraps(f)
+    def decorated(*args, **kwargs):
+        token = request.args.get('token')
+
+        # ensure that token is specified in the request
+        if not token:
+            return jsonify({'message': 'Missing token!'})
+
+        # ensure that token is valid
+        try:
+            data = jwt.decode(token, app.config['secretkey'])
+        except:
+            return jsonify({'message': 'Invalid token!'})
+
+        return f(*args, **kwargs)
+
+    return decorated
+
 @app.route('/unprotected')
 def unprotected():
     return 'unprotected'
 
 @app.route('/protected')
+@token_required
 def protected():
     return 'protected'
 
@@ -22,7 +42,11 @@ def login():
 
     if auth and auth.password == 'password':
         expiry = datetime.datetime.utcnow() + datetime.timedelta(minutes=30)
-        token = jwt.encode({'user': user.username, 'exp': expiry})
+        token = jwt.encode({'user': auth.username, 'exp': expiry}, app.config['secretkey'], algorithm='HS256')
         return jsonify({'token' : token.decode('UTF-8')})
 
     return make_response('Could not verify!', 401, {'WWW-Authenticate': 'Basic realm="Login Required"'})
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
